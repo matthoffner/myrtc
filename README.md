7884713e96055eda4fdc26c55f4a4bc4140358ca
Matt Hoffner <matthoffner@gmail.com>
2020-10-10 15:46:59 -0700

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# myrtc
+
+Simple locally hosted video chat with ngrok.
+
+```sh
+npm install
+```
+
+```sh
+npm run prepare-client
+npm run prepare-server
+npm run start
+```
+
+## Credits
+
+[video-group-meeting](https://github.com/Hyunse/video-group-meeting)
+[ngrok]()
+[simple-peer]()
+[socket.io]()
---
+++ client/build/precache-manifest.1cf04ee56597268adb96c65e139b221a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf408966338ac10d64b1e658d2560738",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29b75f445baa36e8988d",
+    "url": "/static/css/main.e081a844.chunk.css"
+  },
+  {
+    "revision": "6bb723b09893b9589a9b",
+    "url": "/static/js/2.2b1bbe9a.chunk.js"
+  },
+  {
+    "revision": "78b287351bb08f453fa0918ea10ac39c",
+    "url": "/static/js/2.2b1bbe9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29b75f445baa36e8988d",
+    "url": "/static/js/main.e5b9383e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
